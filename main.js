5b603bf73dfa7de851d0806cfbc8389743f60817
Danijel Dzakula <danidzaki060@gmail.com>
2018-09-13 20:49:25 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+var name = 'bob',
+    email = 'danijeldzakula1991@gmail.com',
+    comments = 'yo, fresh';
+
+function sendForm() {
+  
+  var data = {
+    name:name,
+    email:email,
+    comments:comments,
+    _subject:'My Form Submission',
+  }
+
+  var request = new XMLHttpRequest();
+  request.open('POST', 'https://formspree.io/danijeldzakula1991@gmail.com', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(data);
+}
+
+let submit = document.querySelector('.js-contact-form-submit');
+submit.addEventListener('click', sendForm());
